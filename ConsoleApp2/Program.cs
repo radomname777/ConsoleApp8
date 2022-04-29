39b924b69f7216f81d70a4c02ed52ef0300771cb
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-04-30 02:58:34 +0400

    0.2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -17,8 +17,8 @@ namespace MyApp
                     string[][] ss = JsonSerializer.Deserialize<string[][]>(item[0]);
                     foreach (var item1 in ss)
                     {
-                        if (item1[0].ToUpper() == country.ToUpper())return item1[1];
-                        else if(item1[1].ToUpper() == country.ToUpper()) return item1[0];
+                        if (item1[0].ToUpper() == country.ToUpper()) return item1[1];
+                        else if (item1[1].ToUpper() == country.ToUpper()) return item1[0];
                     }
                 }
                 return "Tapilmadi";
@@ -30,22 +30,29 @@ namespace MyApp
         static void Main(string[] args)
         {
             World dictionary = new World();
-            dictionary.Dictionary = new string[][]{
-                new string[2] { "Hello", "Salam"},new string[2] { "How", "Nece"},
-                new string[2] { "You", "Sen"},    new string[2] { "World", "Dunya"},
-                new string[2] { "We", "Biz"},     new string[2] { "Home", "Ev"},
-                new string[2] { "Dog", "it"},     new string[2] { "Cat", "pisik"},
-                new string[2] { "Parrot", "tutuqusu"}, new string[2] { "Ocean", "Okean"},
-                new string[2] { "War", "Muharibe"},    new string[2] { "flame", "Alow"},
-                new string[2] { "Car", "masin"},       new string[2] { "Caspian", "Xezer"},
-                new string[2] { "flower", "Gul"},      new string[2] { "Mountain", "Dag"},
-                new string[2] { "Sun", "Gunes"},       new string[2] { "Computer", "Komputer"},
-                new string[2] { "Qaz", "Goose"},       new string[2] { "COw", "Inek"}
-            };
-            var json = JsonSerializer.Serialize(dictionary.Dictionary);
-            File.WriteAllText("dictionary.txt", json + '\n');
-            Console.WriteLine(dictionary["you"]);
-            Console.WriteLine(dictionary["sen"]);
+            // file
+            ////dictionary.Dictionary = new string[][]{
+            ////    new string[2] { "Hello", "Salam"},     new string[2] { "How", "Nece"},
+            ////    new string[2] { "You", "Sen"},         new string[2] { "World", "Dunya"},
+            ////    new string[2] { "We", "Biz"},          new string[2] { "Home", "Ev"},
+            ////    new string[2] { "Dog", "it"},          new string[2] { "Cat", "pisik"},
+            ////    new string[2] { "Parrot", "tutuqusu"}, new string[2] { "Ocean", "Okean"},
+            ////    new string[2] { "War", "Muharibe"},    new string[2] { "flame", "Alow"},
+            ////    new string[2] { "Car", "masin"},       new string[2] { "Caspian", "Xezer"},
+            ////    new string[2] { "flower", "Gul"},      new string[2] { "Mountain", "Dag"},
+            ////    new string[2] { "Sun", "Gunes"},       new string[2] { "Computer", "Komputer"},
+            ////    new string[2] { "Qaz", "Goose"},       new string[2] { "Cow", "Inek"}
+            ////};
+            ////var json = JsonSerializer.Serialize(dictionary.Dictionary);
+            ////File.WriteAllText("dictionary.txt", json + '\n');
+            while (true)
+            {
+                Console.Write("Enter: ");
+                string name = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine(dictionary[name]);
+            }
+
         }
     }
 }
---
+++ country/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.Json;
+
+namespace MyApp
+{
+    class Capital
+    {
+        public double Territory { get; set; }
+        public int Population { get; set; }
+        public string Name { get; set; }
+        public Capital(double territory, int population, string name)
+        {
+            Territory = territory;
+            Population = population;
+            Name = name;
+
+        }
+        public override string ToString() { return $"Capital: {Name}\nPopulation: {Population}\nTerritory: {Territory}KM"; }
+    }
+    class Country
+    {
+        public double Territory { get; set; }
+        public int Population { get; set; }
+        public string Name { get; set; }
+        public Capital Capital { get; set; }
+        public Country(double territory, int population, string name, Capital cap)
+        {
+            Territory = territory;
+            Population = population;
+            Name = name;
+            Capital = cap;
+        }
+        public static bool operator <(Country country1, Country country2)
+        {
+
+            if (country1.Population < country2.Population)
+            {
+                Console.WriteLine(country2.Capital);
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >(Country country1, Country country2)
+        {
+            if (country1.Capital.Population > country2.Capital.Population)
+            {
+                Console.WriteLine(country1.Capital);
+                return true;
+            }
+            return false;
+        }
+        public override string ToString() { return $"Country: {Name}\nPopulation: {Population}\nTerritory: {Territory}KM\n{Capital}"; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Country Coun = new Country(86600, 10173200, "Azerbaycan", new Capital(2140, 2300500, "Baki"));
+            Country Coun2 = new Country(86600, 10173200, "Azerbaycan", new Capital(170, 300600, "Gence"));
+            Console.WriteLine(Coun > Coun2);
+            Console.WriteLine(Coun);
+        }
+    }
+}
